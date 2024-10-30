45c734feb300794046a5f7bf97d9e8fcbca87546
Juan Pablo Castaño <jpc4stano@gmail.com>
2024-10-30 14:31:15 -0500

    Quitando el token del local storage

--- src/Redux/States/auth.ts
+++ src/Redux/States/auth.ts
@@ -5,27 +5,19 @@ export const EmptyAuthState: AuthInfo = {
   token: "",
 };
 
-export const persistLocalStoregeAuth = (authInfo: AuthInfo) => {
-  localStorage.setItem("auth", JSON.stringify({ ...authInfo }));
-};
 
 export const authSlice = createSlice({
   name: "auth",
-  initialState: localStorage.getItem("auth")
-    ? JSON.parse(localStorage.getItem("auth") as string)
-    : EmptyAuthState,
+  initialState: EmptyAuthState,
   reducers: {
     createAuth: (state, action) => {
-      persistLocalStoregeAuth(action.payload);
       return action.payload;
     },
     updateAuth: (state, action) => {
       const result = { ...state, ...action.payload };
-      persistLocalStoregeAuth(result)
       return result;
     },
     resetAuth: () => {
-      localStorage.removeItem("auth")
       return EmptyAuthState;
     },
   },
